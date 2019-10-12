aeb3cc52bc70afc80240929e9b5107f6206eab73
Kostas Bakogiannis <kosbako10@gmail.com>
2019-10-12 18:13:53 +0300

    cocktail routes changes

--- src/api/routes/cocktail.route.js
+++ src/api/routes/cocktail.route.js
@@ -1,53 +1,53 @@
 const express = require('express');
 const {
-    searchCocktailByName,
-    getCocktailDetails,
-    getIngredientDetails,
-    getRandomCocktail,
-    searchByIngredientName,
-    getList,
-    filterByCategory,
-    filterByAlcoholic,
-    filterByGlass
+  searchCocktailByName,
+  getCocktailDetails,
+  getIngredientDetails,
+  getRandomCocktail,
+  searchByIngredientName,
+  getList,
+  filterByCategory,
+  filterByAlcoholic,
+  filterByGlass
 } = require('../controllers/cocktail.controller');
 
 const cocktailRouter = express.Router();
 
-/** 
+/**
  * Search cocktails
  */
 cocktailRouter.get('/search/:name', searchCocktailByName);
 /**
  * Get Single cocktail by id
  */
-cocktailRouter.get('/single/:id', getCocktailDetails)
+cocktailRouter.get('/single/:id', getCocktailDetails);
 /**
- * Get single ingredient by id
+ * Get single ingredient by name
  */
-cocktailRouter.get('/ingredient/single/:id', getIngredientDetails)
+cocktailRouter.get('/ingredient/single/:name', getIngredientDetails);
 /**
  * Search cocktails by ingredient name
  */
-cocktailRouter.get('/ingredient/search/:name', searchByIngredientName)
+cocktailRouter.get('/ingredient/search/:name', searchByIngredientName);
 /**
  * Get a random cocktail
  */
-cocktailRouter.get('/random', getRandomCocktail)
+cocktailRouter.get('/random', getRandomCocktail);
 /**
  * Get categories, glasses, ingredient, alcoholic by query (name)
  */
-cocktailRouter.get('/list', getList)
+cocktailRouter.get('/list', getList);
 /**
  * Filter cocktails by category name
  */
-cocktailRouter.get('/filter/category/:name', filterByCategory)
+cocktailRouter.get('/filter/category/:name', filterByCategory);
 /**
  * Filter cocktails by alcoholic name
  */
-cocktailRouter.get('/filter/alcoholic/:name', filterByAlcoholic)
+cocktailRouter.get('/filter/alcoholic/:name', filterByAlcoholic);
 /**
  * Filter cocktails by glass name
  */
-cocktailRouter.get('/filter/glass/:name', filterByGlass)
+cocktailRouter.get('/filter/glass/:name', filterByGlass);
 
 module.exports = cocktailRouter;
--- src/services/cocktailServices.js
+++ src/services/cocktailServices.js
@@ -1,48 +1,63 @@
-const axios = require('axios')
-const { cocktailDBUrl } = require('../config/keys')
+const axios = require('axios');
+const { cocktailDBUrl } = require('../config/keys');
 
+exports.getCoctailByName = async name => {
+  const searchResponse = await axios.get(
+    `${cocktailDBUrl}/search.php?s=${name}`
+  );
+  return searchResponse.data;
+};
 
-exports.getCoctailByName = async (name) => {
-    const searchResponse = await axios.get(`${cocktailDBUrl}/search.php?s=${name}`)
-    return searchResponse.data
-}
+exports.getCoctailById = async id => {
+  const singleCocktailResponseResponse = await axios.get(
+    `${cocktailDBUrl}/lookup.php?i=${id}`
+  );
+  return singleCocktailResponseResponse.data;
+};
 
-exports.getCoctailById = async (id) => {
-    const singleCocktailResponseResponse = await axios.get(`${cocktailDBUrl}/lookup.php?i=${id}`)
-    return singleCocktailResponseResponse.data
-}
-
-exports.getIngredientById = async (id) => {
-    const singleIngredientResponse = await axios.get(`${cocktailDBUrl}/lookup.php?iid=${id}`)
-    return singleIngredientResponse.data
-}
+exports.getIngredientById = async name => {
+  const singleIngredientResponse = await axios.get(
+    `${cocktailDBUrl}/search.php?i=${name}`
+  );
+  return singleIngredientResponse.data;
+};
 
 exports.getRandomCocktail = async () => {
-    const randomCocktailResponse = await axios.get(`${cocktailDBUrl}/random.php`)
-    return randomCocktailResponse.data
-}
+  const randomCocktailResponse = await axios.get(`${cocktailDBUrl}/random.php`);
+  return randomCocktailResponse.data;
+};
 
-exports.getCocktailByIngredientName = async (name) => {
-    const ingredientSearchResponse = await axios.get(`${cocktailDBUrl}/filter.php?i=${name}`)
-    return ingredientSearchResponse.data
-}
+exports.getCocktailByIngredientName = async name => {
+  const ingredientSearchResponse = await axios.get(
+    `${cocktailDBUrl}/filter.php?i=${name}`
+  );
+  return ingredientSearchResponse.data;
+};
 
-exports.getListByQuery = async (query) => {
-    const listResponse = await axios.get(`${cocktailDBUrl}/list.php?${query}=list`)
-    return listResponse.data
-}
+exports.getListByQuery = async query => {
+  const listResponse = await axios.get(
+    `${cocktailDBUrl}/list.php?${query}=list`
+  );
+  return listResponse.data;
+};
 
-exports.filterCocktailsByCategory = async (name) => {
-    const cocktailResponse = await axios.get(`${cocktailDBUrl}/filter.php?c=${name}`)
-    return cocktailResponse.data
-}
+exports.filterCocktailsByCategory = async name => {
+  const cocktailResponse = await axios.get(
+    `${cocktailDBUrl}/filter.php?c=${name}`
+  );
+  return cocktailResponse.data;
+};
 
-exports.filterCocktailsByAlcoholic = async (name) => {
-    const cocktailResponse = await axios.get(`${cocktailDBUrl}/filter.php?a=${name}`)
-    return cocktailResponse.data
-}
+exports.filterCocktailsByAlcoholic = async name => {
+  const cocktailResponse = await axios.get(
+    `${cocktailDBUrl}/filter.php?a=${name}`
+  );
+  return cocktailResponse.data;
+};
 
-exports.filterCocktailsByGlass = async (name) => {
-    const cocktailResponse = await axios.get(`${cocktailDBUrl}/filter.php?g=${name}`)
-    return cocktailResponse.data
-}
+exports.filterCocktailsByGlass = async name => {
+  const cocktailResponse = await axios.get(
+    `${cocktailDBUrl}/filter.php?g=${name}`
+  );
+  return cocktailResponse.data;
+};
